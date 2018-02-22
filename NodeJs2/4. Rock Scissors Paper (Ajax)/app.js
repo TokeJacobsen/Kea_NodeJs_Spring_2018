01ca7b2184ac87b4e23220483462b183cfb487e8
Anders Latif <anders@github.com>
2018-02-22 15:47:48 +0100

    final commit of the day for the rock paper scissors code class 2

--- NodeJs2/4. Rock Scissors Paper (Ajax)/app.js
+++ NodeJs2/4. Rock Scissors Paper (Ajax)/app.js
@@ -3,10 +3,29 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"))
 
+var bodyParser = require("body-parser");
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+var playerOne = {"playerId": 1, "nickname": ""};
+var playerTwo = {"playerId": 2, "nickname": ""};
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/public/index.html");
 });
+
+
+
+app.post("/register-user", function(req, res) {
+    console.log(req.body);
+    res.json(req.body);
+});
+
+
 
 var server = app.listen("3000", function(err) {
     if (err) {
