c5f9abba05501fce65995e637119178fa1ba37a8
Anders Latif <anders@github.com>
2018-03-15 12:48:07 +0100

    first assignment with fs nodejs2

---
+++ NodeJs2/7.1 Advanced Node.js II/aboutBob.js
@@ -0,0 +1,13 @@
+class BobStruction {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    bobTangle() {
+        return this.x * this.y;
+    }
+}
+
+
+
+module.exports = BobStruction;
---
+++ NodeJs2/7.1 Advanced Node.js II/main.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const Bob = require("./aboutBob.js");
+
+let myBob = new Bob(3, 6);
+console.log(myBob.bobTangle());
+
