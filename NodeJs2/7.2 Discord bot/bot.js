662ed7b6a368a3a14983c53233b473f964d1eda9
Anders Latif <anders@github.com>
2018-03-15 15:14:44 +0100

    final discord bot version for nodejs2

--- NodeJs2/7.2 Discord bot/bot.js
+++ NodeJs2/7.2 Discord bot/bot.js
@@ -1,14 +1,38 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
+const botsettings = require("./botsettings.json")
  
-client.on('ready', () => {
+
+client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag}!`);
+
+    // try {
+    //     let link = await client.generateInvite(["ADMINISTRATOR"]);
+    //     console.log("This is the link: ", link);
+    // } catch(err) {
+    //     console.log(err);
+    // }
+
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
+    if (msg.author.bot) {
+        return;
+    }
+
+
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+    }
+
+    let words = msg.content.split(" ");
+
+    if (words.indexOf("bot") >= 0 && words.indexOf("talk") >= 0) {
+        msg.react("👌");
+        msg.reply("I'm just a silly bot. \:tired_face:");
+    }
+
 });
  
-client.login('token');
+client.login(botsettings.token);
