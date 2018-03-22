640585b639c638df431c49681d0391fd19185e56
Anders Latif <anders@github.com>
2018-03-22 15:33:53 +0100

    Before implementing the queries nodejs2

--- NodeJs2/8._Mysql/app.js
+++ NodeJs2/8._Mysql/app.js
@@ -9,9 +9,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+const objection = require("objection");
+const Model = objection.Model;
+const Knex = require("knex"); 
+const knexConfig = require("./knexfile.js");
 
-app.post("/get-messages", function(req, res) {
+// Initialize knex with the connection configurations
+const knex = Knex(knexConfig.development);
+
+// Giving the knex connection to objection.js combining the two
+Model.knex(knex);
+
+// Convenience object that contains all the Models
+const db = {
+    "Knex": knex,
+    "Message": require("./models/Message.js")
+};
+// the body of this post should have a message key
+app.post("/add-message", function(req, res) {
     let response = {};
+
+    response.status = 200;
+    res.send(response);
+});
+
+
+
+app.get("/get-messages", function(req, res) {
+    let response = {};
+
+
 
     response.status = 200;
     res.send(response);
---
+++ NodeJs2/8._Mysql/models/Message.js
@@ -0,0 +1,9 @@
+const Model = require('objection').Model;
+
+class Message extends Model {
+    static get tableName() {
+        return 'messages';
+    }
+}
+
+module.exports = Message;
